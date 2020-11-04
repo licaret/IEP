550fa457dd4735e52d6fa44022e21fc69135e76c
licaret <35605618+licaret@users.noreply.github.com>
2020-11-04 21:23:39 +0200

    Item 4 resolved

--- Class.cpp
+++ Class.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 
+class Clock
+{
+private:
+    int hour;
+    int minute;
+    int second;
+
+public:
+    Clock(int hour, int minute, int second)
+        : hour(hour),
+          minute(minute),
+          second(second)
+    {}
+};
+
 int main()
 {
 
